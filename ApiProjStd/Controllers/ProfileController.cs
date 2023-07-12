64128db8dbfdb1e7a3cbc31aed84098ba347e64e
eduardoccarmo <eduardo.ccarmo@outlook.com>
2023-07-11 22:15:02 -0300

    Implementando getById no profile Controller.

--- ApiProjStd/Controllers/ProfileController.cs
+++ ApiProjStd/Controllers/ProfileController.cs
@@ -14,7 +14,7 @@ namespace ApiProjStd.Controllers
         private readonly IProfileService _profileService;
 
         public ProfileController(IProfileService profileService)
-        { 
+        {
             _profileService = profileService;
         }
 
@@ -34,6 +34,25 @@ namespace ApiProjStd.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, new ResultViewModel<Profile>(errors: "An error has ocured."));
+            }
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var profile = await _profileService.GetAsync(id);
+
+                if(profile is not null)
+                    return Ok(profile);
+
+                return BadRequest(new ResultViewModel<Profile>(errors: "Profile is not found."));
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new ResultViewModel<Profile>(errors: "An erros has ocured."));
             }
         }
     }
