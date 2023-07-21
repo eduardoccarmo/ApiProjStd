0dc94f803e150b1db126d120dbd2e6d4235dc237
eduardoccarmo <eduardo.ccarmo@outlook.com>
2023-07-21 16:42:11 -0300

    Implementando UserServices,  metodos Get.

--- Api.Proj.Application/Interface/IUserService.cs
+++ Api.Proj.Application/Interface/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Api.Proj.Std.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Api.Proj.Std.Application.Interface
 {
     public interface IUserService
     {
+        Task<User> GetByName(string name);
+
+        Task<List<User>> GetAll();
     }
 }
--- Api.Proj.Application/Services/UserService.cs
+++ Api.Proj.Application/Services/UserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Api.Proj.Std.Application.Interface;
+using Api.Proj.Std.Domain.Models;
+using Api.Proj.Std.Domain.Models.IRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,34 @@ using System.Threading.Tasks;
 
 namespace Api.Proj.Std.Application.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
 
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            var users = await _userRepository.GetAll();
+
+            if (users is not null)
+                return users;
+
+            return null;
+
+        }
+
+        public async  Task<User> GetByName(string name)
+        {
+           var user = await _userRepository.GetByName(name);
+
+            if (!(user == null))
+                return user;
+
+            return null;
+        }
     }
 }
